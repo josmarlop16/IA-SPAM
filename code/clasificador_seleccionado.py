110666cd318da9ff466cfae76fa3880000326a09
Josmarlop16 <josmarlop16@alum.us.es>
2022-06-08 15:06:28 +0200

    avances

--- code/clasificador_seleccionado.py
+++ code/clasificador_seleccionado.py
@@ -1,2 +1,16 @@
+import email_csv_parser
+from naive_bayes_multinomial import NB_classifier_alpha_1, bag_of_words_tokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+
 def es_mensaje_no_deseado(path):
-    pass
+    parsedEmail = email_csv_parser.parse_email2(path)
+    cleanedEmail = email_csv_parser.clean_email(parsedEmail)
+    cv = CountVectorizer(analyzer=bag_of_words_tokenizer)
+    pred = cv.transform(cleanedEmail)
+    prediction = NB_classifier_alpha_1.predict(pred)
+    if prediction[0] == 0:
+        return False
+    else:
+        return True
+
+print(es_mensaje_no_deseado(r"Enron-Spam/legítimo/0"))
---
+++ code/email_csv_parser.py
@@ -0,0 +1,111 @@
+import email
+import os
+from base64 import decode
+from email import policy
+import csv
+import splitfolders
+
+# splitfolders.fixed(r'Enron-Spam/',output="src", seed=42, fixed=(500, 500), oversample=False, group_prefix=None)
+
+# Obtain the emails from the folder
+listLegit = os.listdir(r"src/test/legítimo")
+listSpam = os.listdir(r"src/test/no_deseado")
+
+# Email parser function
+def parse_email(list, path):
+    # Create a list to store the emails
+    list_body = []
+    # Iterate through the emails
+    for i in list:
+        # read the email
+        with open(path + "/" + i, encoding="latin-1") as f:
+            # get the body
+            msg = email.message_from_file(f)
+            if msg.is_multipart():
+                for part in msg.walk():
+                    try:
+                        payload = part.get_payload(decode=True)
+                        # returns a bytes object
+                        strtext = payload.decode("latin-1")
+                    except:
+                        strtext = part.get_payload(decode=False)
+                        # returns a bytes object
+                    list_body.append(strtext)
+            else:
+                try:
+                    payload = msg.get_payload(decode=True)
+                    strtext = payload.decode("latin-1")
+                except:
+                    strtext = msg.get_payload(decode=False)
+                list_body.append(strtext)
+
+    return list_body
+
+# Email parser function
+
+
+def parse_email2(path):
+    # Create a list to store the email
+    list_body = []
+    # read the email
+    with open(path , encoding="latin-1") as f:
+        # get the body
+        msg = email.message_from_file(f)
+        if msg.is_multipart():
+            for part in msg.walk():
+                try:
+                    payload = part.get_payload(decode=True)
+                    # returns a bytes object
+                    strtext = payload.decode("latin-1")
+                except:
+                    strtext = part.get_payload(decode=False)
+                    # returns a bytes object
+                list_body.append(strtext)
+        else:
+            try:
+                payload = msg.get_payload(decode=True)
+                strtext = payload.decode("latin-1")
+            except:
+                strtext = msg.get_payload(decode=False)
+            list_body.append(strtext)
+    return list_body
+
+
+def clean_email(email):
+    # remove html tags and \n from an email
+    # create a list to store the cleaned email
+    # remove invalid emails
+    cleaned_email = []
+    for i in email:
+        try:
+            email = i.replace("<", "").replace(">", "").replace("\n", "")
+            cleaned_email.append(email)
+        except:
+            # Invalid email is ignored
+            pass
+    return cleaned_email
+
+# Parsing the emails
+list_body_legit = parse_email(listLegit, r"src/test/legítimo")
+list_body_spam = parse_email(listSpam, r"src/test/no_deseado")
+# Cleaning the emails
+list_body_legit_cleaned = clean_email(list_body_legit)
+list_body_spam_cleaned = clean_email(list_body_spam)
+
+############### Creating CSV files ###################
+rowHeaders = ["Email", "isSpam"]
+
+with open("spam.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(rowHeaders)
+    for i in range(len(list_body_spam_cleaned)):
+        writer.writerow([list_body_spam_cleaned[i], "1"])
+    f.close()
+
+with open("legit.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(rowHeaders)
+    for i in range(len(list_body_legit_cleaned)):
+        writer.writerow([list_body_legit_cleaned[i], "0"])
+    f.close()
+####################################################
