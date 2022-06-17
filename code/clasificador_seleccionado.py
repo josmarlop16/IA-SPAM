65b15bd2d1b7933865c3830335b971a7f850dab6
Josmarlop16 <josmarlop16@alum.us.es>
2022-06-17 13:49:41 +0200

    actualiza

--- code/clasificador_seleccionado.py
+++ code/clasificador_seleccionado.py
@@ -1,37 +1,55 @@
 from email_csv_parser import preproccessEmail
-from naive_bayes_multinomial import NB_classifier_alpha_1, NB_classifier_alpha_2, NB_classifier_alpha_3, cv
-from tf_idf import tfidf_vectorizer, Knn_Classifier_n5
+import _pickle as cPickle
+from vectorizer import bag_of_words_tokenizer
 
-def es_mensaje_no_deseado(path):
+nbmA15 = cPickle.load(open('nbm.pickle', 'rb'))
+cv = cPickle.load(open('vectorizer.pickle', 'rb'))
+
+knn = cPickle.load(open('knn.pickle', 'rb'))
+feat = cPickle.load(open('knnvectorizer.pickle', 'rb'))
+
+
+def es_mensaje_no_deseado_NB(path):
     # Preproccess the email
     transformed_text = preproccessEmail(path)
     # Vectorize the email
+    cv.set_params(analyzer=bag_of_words_tokenizer)
     vector_input = cv.transform(transformed_text)
     # Predict the email
-    result = NB_classifier_alpha_3.predict(vector_input)
-    # Print the email
-    print(transformed_text)
+    result = nbmA15.predict(vector_input)
     # [0:NotSpam, 1:Spam]
-    if (result == 0):
+    if result == 0:
         return False
     else:
         return True
 
-print("Naive Bayes Detector -> ¿Este correo es spam? ",es_mensaje_no_deseado(r"src/test/no_deseado/9"), "\n")
 
-def es_mensaje_no_deseado_tf_idf(path):
+print(
+    "Naive Bayes Detector -> ¿Este correo es spam? ",
+    es_mensaje_no_deseado_NB(r"src/test/no_deseado/604"),
+    "\n",
+)
+
+
+def es_mensaje_no_deseado_kNN(path):
     # Preproccess the email
     transformed_text = preproccessEmail(path)
     # Vectorize the email
-    vector_input = tfidf_vectorizer.transform(transformed_text)
+    feat.set_params(analyzer=bag_of_words_tokenizer)
+    vector_input = feat.transform(transformed_text)
     # Predict the email
-    result = Knn_Classifier_n5.predict(vector_input)
+    result = knn.predict(vector_input)
     # Print the email
     print(transformed_text)
     # [0:NotSpam, 1:Spam]
-    if (result == 0):
+    if result == 0:
         return False
     else:
         return True
 
-print("tf-idf Detector -> ¿Este correo es spam? ",es_mensaje_no_deseado_tf_idf(r"src/test/no_deseado/9"), "\n")
+
+print(
+    "kNN Detector -> ¿Este correo es spam? ",
+    es_mensaje_no_deseado_kNN(r"src/test/no_deseado/604"),
+    "\n",
+)
